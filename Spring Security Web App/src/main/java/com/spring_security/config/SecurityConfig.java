b2981889611424f6ca21a22590a2fc50358dd1c9
fuadik88 <fuadik88@gmail.com>
2021-09-05 21:47:23 +0400

    revision

--- Spring Security Web App/src/main/java/com/spring_security/config/SecurityConfig.java
+++ Spring Security Web App/src/main/java/com/spring_security/config/SecurityConfig.java
@@ -21,6 +21,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-
+        http.authorizeRequests().anyRequest().authenticated().and()
+                .formLogin().loginPage("/signin")
+                .loginProcessingUrl("/auth").permitAll();
     }
 }
